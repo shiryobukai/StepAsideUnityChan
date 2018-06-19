a86d332625407c24ff75037aef4f45dcde26df8e
shiryobukai <s1513087.logic@gmail.com>
2018-06-20 06:28:53 +0900

    destroy objects
    ユニティちゃんの後ろのオブジェクトを消す

---
+++ Assets/ObjectController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour {
+    private GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+		
+		       //Unityちゃんのオブジェクトを取得
+                this.unitychan = GameObject.Find ("unitychan");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.z - unitychan.transform.position.z < -5){
+			Destroy(this.gameObject);
+		}
+	}
+}
